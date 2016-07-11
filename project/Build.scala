e4df7cdb945f5bab81b4001c514d5bfac42db9bb
masato <goodmoon.badmoon@gmail.com>
2016-07-12 00:39:38 +0900

    added a method to create command file.

--- project/Build.scala
+++ project/Build.scala
@@ -1,5 +1,5 @@
 import java.io.File
-import sbt.Keys._
+
 import sbt._
 import sbtassembly.AssemblyPlugin
 import sbtassembly.AssemblyPlugin.assemblySettings
@@ -7,16 +7,29 @@ import sbtassembly.AssemblyKeys
 import sbtassembly.AssemblyPlugin.autoImport._
 
 object Build extends Build {
+  val pathToCommandTemplete = "./project/resources/templete/ergit"
+  val outDir = "./out/artifacts/ergit/"
+  val commandFileName = "ergit"
+
   val packageErGit = taskKey[Unit]("package ErGit.")
 
   // Project settings
   lazy val root = (project in file(".")).
     settings(
-      packageErGit := {},
-      packageErGit <<= packageErGit.dependsOn(assembly map {(file: File) =>
+      packageErGit := {}
+      , packageErGit <<= packageErGit.dependsOn(assembly map { (file: File) =>
         println("run packageErGit")
-
-        println(file.name)
+        packaging(file)
       })
     )
+
+  def packaging(jarFile: File): Unit = {
+    val commandText = createCommandText(jarFile.name)
+    val outCommandFile = new File(outDir, commandFileName)
+    IO.write(outCommandFile, commandText, IO.utf8, append = false)
+  }
+
+  def createCommandText(jarName: String): String = {
+    IO.read(new File(pathToCommandTemplete), IO.utf8).replace("__JAR__", jarName)
+  }
 }
