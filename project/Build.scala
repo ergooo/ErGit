ccaa46c899d0ef8f7944d8f7a77e5dd82a4bb88e
masato <goodmoon.badmoon@gmail.com>
2016-07-13 00:48:35 +0900

    enabled to create an executable command with an executable jar.

--- project/Build.scala
+++ project/Build.scala
@@ -1,35 +1,34 @@
 import java.io.File
 
 import sbt._
-import sbtassembly.AssemblyPlugin
-import sbtassembly.AssemblyPlugin.assemblySettings
-import sbtassembly.AssemblyKeys
 import sbtassembly.AssemblyPlugin.autoImport._
 
 object Build extends Build {
   val pathToCommandTemplete = "./project/resources/templete/ergit"
-  val outDir = "./out/artifacts/ergit/"
+  val artifactDirectory = "./out/artifacts/"
   val commandFileName = "ergit"
 
-  val packageErGit = taskKey[Unit]("package ErGit.")
+  val packagingDirectoryName: Def.Initialize[String] = Def.setting {
+    "%s-%s".format(Keys.name.value, Keys.version.value)
+  }
 
+  val packageErGit = taskKey[Unit]("package ErGit.")
+  
   // Project settings
   lazy val root = (project in file(".")).
     settings(
-      packageErGit := {}
-      , packageErGit <<= packageErGit.dependsOn(assembly map { (file: File) =>
-        println("run packageErGit")
-        packaging(file)
-      })
+      packageErGit := {
+        val jarFile = assembly.value
+
+        val pathToPackagingDirectory = artifactDirectory + packagingDirectoryName.value
+        val commandText = IO.read(new File(pathToCommandTemplete), IO.utf8).replace("__JAR__", jarFile.name)
+        val outCommandFile = new File(pathToPackagingDirectory, commandFileName)
+        outCommandFile.setExecutable(true)
+        IO.write(outCommandFile, commandText, IO.utf8, append = false)
+
+        IO.copyFile(jarFile, new File(pathToPackagingDirectory, jarFile.name))
+      }
     )
 
-  def packaging(jarFile: File): Unit = {
-    val commandText = createCommandText(jarFile.name)
-    val outCommandFile = new File(outDir, commandFileName)
-    IO.write(outCommandFile, commandText, IO.utf8, append = false)
-  }
 
-  def createCommandText(jarName: String): String = {
-    IO.read(new File(pathToCommandTemplete), IO.utf8).replace("__JAR__", jarName)
-  }
 }
