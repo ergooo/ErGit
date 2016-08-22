555bc1a3f916b5fa4cf72daa310bdbada7dc6c16
masato <goodmoon.badmoon@gmail.com>
2016-08-23 00:36:52 +0900

    fixed the console message.

--- modules/core/src/main/scala/jp.ergo.ergit/core/domain/status/Status.scala
+++ modules/core/src/main/scala/jp.ergo.ergit/core/domain/status/Status.scala
@@ -1,6 +1,6 @@
 package jp.ergo.ergit.core.domain.status
 
-case class Status(states: Seq[State], displayableString: String) {
+case class Status(states: Seq[State], displayableString: String, porcelainString: String) {
   def hasNoChange = states.isEmpty
 }
 
@@ -8,12 +8,12 @@ object Status {
 
   def apply(porcelain: String, displayableString: String) = {
     porcelain match {
-      case "" => new Status(Seq(), displayableString)
+      case "" => new Status(Seq(), displayableString, porcelain)
       case _ => {
         val states = porcelain.split("\n") map { line =>
           StateParser.parse(line)
         }
-        new Status(states, displayableString)
+        new Status(states, displayableString, porcelain)
       }
     }
   }
